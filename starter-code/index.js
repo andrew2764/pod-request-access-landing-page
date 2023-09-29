dd2d09a742db4a9e846ca06d9126f5c4cbdac29d
andrew <andrew2764@gmail.com>
2023-09-29 13:18:25 +0800

    Add validation for missing email

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,12 +4,18 @@ const errorMessageElem = document.createElement("span");
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 errorMessageElem.classList.add("error");
-errorMessageElem.innerText = "Opps! Please check your email!";
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   document.querySelector(".error")?.remove();
+  if (!input.value) {
+    errorMessageElem.innerText = "Opps! Please add your email!";
+    form.appendChild(errorMessageElem);
+    return;
+  }
   const regexResult = input.value.toLowerCase().match(emailRegex);
   if (!regexResult) {
+    errorMessageElem.innerText = "Opps! Please check your email!";
     form.appendChild(errorMessageElem);
     return;
   }
